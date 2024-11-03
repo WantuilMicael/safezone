b8760c6064efa68dc2b80c21f932903cd2940862
WantuilMicael <wantuimicael@hotmail.com>
2024-11-02 22:20:57 -0300

    Criação do popup de registrar ocorrencia

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
+// MapSection.js
+import React, { useState } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import RegistroOcorrencia from '../RegistroOcorrencia'; // Importe o componente do popup
 import './MapSection.css';
 
 const MapSection = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const togglePopup = () => {
+    setIsPopupOpen(!isPopupOpen);
+  };
+
   const mapContainerStyle = {
     height: '400px',
     width: '100%',
@@ -17,7 +25,7 @@ const MapSection = () => {
     <div className='map-section'>
       <h2 className='map-title'>Mapa</h2>
       <div className='map-button'>
-        <button>Registrar Ocorrência</button>
+        <button onClick={togglePopup}>Registrar Ocorrência</button>
       </div>
       <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
         <GoogleMap
@@ -30,37 +38,38 @@ const MapSection = () => {
       </LoadScript>
 
       <div className='map-info'>
-        {
-          <div className='tables-container'>
-            <div className='table'>
-              <div className='table-row'>
-                <div className='table-cell'>Total de registros</div>
-              </div>
-              <div className='table-row second-row'>
-                <div className='table-cell'>1.683</div>
-              </div>
+        <div className='tables-container'>
+          <div className='table'>
+            <div className='table-row'>
+              <div className='table-cell'>Total de registros</div>
             </div>
-            <div className='table'>
-              <div className='table-row'>
-                <div className='table-cell'>
-                  População do estado do Rio de Janeiro
-                </div>
-              </div>
-              <div className='table-row second-row'>
-                <div className='table-cell'>16.054.524</div>
-              </div>
-            </div>
-            <div className='table'>
-              <div className='table-row'>
-                <div className='table-cell'>Eventos por habitantes (%)</div>
-              </div>
-              <div className='table-row second-row'>
-                <div className='table-cell'>1,4%</div>
-              </div>
+            <div className='table-row second-row'>
+              <div className='table-cell'>1.683</div>
             </div>
           </div>
-        }
+          <div className='table'>
+            <div className='table-row'>
+              <div className='table-cell'>
+                População do estado do Rio de Janeiro
+              </div>
+            </div>
+            <div className='table-row second-row'>
+              <div className='table-cell'>16.054.524</div>
+            </div>
+          </div>
+          <div className='table'>
+            <div className='table-row'>
+              <div className='table-cell'>Eventos por habitantes (%)</div>
+            </div>
+            <div className='table-row second-row'>
+              <div className='table-cell'>1,4%</div>
+            </div>
+          </div>
+        </div>
       </div>
+
+      {/* Exibir o popup quando `isPopupOpen` for verdadeiro */}
+      {isPopupOpen && <RegistroOcorrencia onClose={togglePopup} />}
     </div>
   );
 };
---
+++ src/components/RegistroOcorrencia/index.js
@@ -0,0 +1,88 @@
+// RegistroOcorrencia.js
+import React, { useState } from 'react'; // Importa useState
+import { IoIosCloseCircle } from 'react-icons/io'; // Importa o ícone
+import './RegistroOcorrencia.css';
+
+const RegistroOcorrencia = ({ onClose }) => {
+  const [selectOpen, setSelectOpen] = useState(false); // Estado para controlar se o select está aberto
+  const [selectedOption, setSelectedOption] = useState(''); // Estado para controlar a opção selecionada
+
+  const handleSelectChange = (event) => {
+    setSelectedOption(event.target.value); // Atualiza a opção selecionada
+    setSelectOpen(false); // Fecha o select após a seleção
+  };
+
+  return (
+    <div className='popup-overlay'>
+      <div className='popup'>
+        <button className='close-button' onClick={onClose}>
+          <IoIosCloseCircle size={24} color='#8F1F1F' />{' '}
+          {/* Ícone na cor vermelha */}
+        </button>
+        <h2 className='popup-title'>Registro de Ocorrência</h2>
+
+        <div className='popup-content'>
+          <div className='input-row'>
+            <div className='input-group'>
+              <label>CEP</label>
+              <input type='text' placeholder='Ex: 12.345-678' />
+            </div>
+
+            <div className='input-group address-input'>
+              <label>Endereço</label>
+              <input type='text' placeholder='Rua, Avenida, etc.' />
+            </div>
+          </div>
+
+          <div className='input-row-triple'>
+            <div className='input-group'>
+              <label>Bairro</label>
+              <input type='text' placeholder='Nome do bairro' />
+            </div>
+
+            <div className='input-group'>
+              <label>Município</label>
+              <input type='text' placeholder='Cidade' />
+            </div>
+
+            <div className='input-group'>
+              <label>Estado</label>
+              <input type='text' placeholder='UF' maxLength={2} />
+            </div>
+          </div>
+
+          <div className='input-row'>
+            <div className='input-group'>
+              <label>Tipo de Ocorrência</label>
+              <select
+                onFocus={() => setSelectOpen(true)} // Define que o select está aberto
+                onBlur={() => setSelectOpen(false)} // Define que o select não está mais aberto
+                onChange={handleSelectChange} // Lida com a mudança de seleção
+                className={selectOpen ? 'select-open' : ''} // Aplica a classe ao select se estiver aberto
+                style={{ color: selectedOption ? 'white' : 'white' }} // Altera a cor do texto baseado na seleção
+                placeholder='Selecione...' // Adiciona um placeholder ao select
+              >
+                <option value=''>Selecione...</option>
+                <option value='Roubo'>Roubo</option>
+                <option value='Furto'>Furto</option>
+                <option value='Sequestro'>Sequestro</option>
+                <option value='Outros'>Outros</option>
+              </select>
+            </div>
+          </div>
+
+          <div className='input-row'>
+            <div className='input-group'>
+              <label>Descrição da Ocorrência</label>
+              <textarea placeholder='Descreva a ocorrência'></textarea>
+            </div>
+          </div>
+
+          <button className='register-button'>Registrar</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegistroOcorrencia;
